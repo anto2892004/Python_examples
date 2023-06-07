e2a48b064ad26c674284fd80c9def04021bb9452
anto2892004 <121860967+anto2892004@users.noreply.github.com>
2023-06-07 12:20:12 +0530

    Add files via upload

---
+++ base_power.py
@@ -0,0 +1,4 @@
+def power(base,pow):
+    a= base**pow
+    print("the exponential of the given is ",a)
+print(power(5,3))
---
+++ string_email.py
@@ -0,0 +1,7 @@
+a=input("enter ypour mail id")
+print("the mail id is",a)
+b =a.split("@")[0]
+print("your username is",c)
+d = b[::-1]
+e =b+d
+print("your password is",e)
